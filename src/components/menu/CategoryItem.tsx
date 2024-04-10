69062ef90be70ee599ee885bf8795c6faa1d74ac
dliang <humga@icloud.com>
2024-04-10 22:53:04 +0800

    children item

---
+++ src/components/menu/CategoryItem.tsx
@@ -0,0 +1,8 @@
+import { Category } from "../data/Category";
+
+const CategoryItem = (props: Category) => {
+  alert(props);
+  return <></>;
+};
+
+export default CategoryItem;
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,6 +1,5 @@
 import { useCallback, useState } from "react";
 // import { CategoryResp } from "../data/Category";
-import { version } from "react-dom/server";
 import { Category } from "../data/Category";
 import CategoryRoot from "./CategoryRoot";
 import { MenuIcon } from "../common/Icon";
@@ -9,11 +8,12 @@ const Menu = () => {
   const [category, setCategory] = useState<[Category]>();
 
   const handleMenuClick = useCallback(() => {
-    category?.push({ name: "WOMEN", level: 0, collapse: true, sub: [] });
-    category?.push({ name: "MEN", level: 0, collapse: true, sub: [] });
-    category?.push({ name: "KIDS", level: 0, collapse: true, sub: [] });
+    category?.map((item, index) => {
+      alert(item);
+      alert(index);
+    });
     setCategory(category);
-  }, [version]);
+  }, []);
 
   category?.map((item) => {
     alert(item);
