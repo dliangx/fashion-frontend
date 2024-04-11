2a961ed6a3b078740ff257f00aa7a60c05a33314
dliang <humga@icloud.com>
2024-04-11 13:46:18 +0800

    categorylist with error

--- src/components/menu/CategoryList.tsx
+++ src/components/menu/CategoryList.tsx
@@ -1,9 +1,22 @@
 import { Category } from "../data/Category";
+import CategoryItem from "./CategoryItem";
 
-function CategoryList(props:Category, children: React.ReactNode) {
-  const handListClick = () => {};
-  alert(props);
-  return <div onClick={handListClick}>{children}</div>;
+function CategoryList(props: Category) {
+  const handListClick = (props:Category) => {
+    renderChildren(props.sub);
+  };
+  function renderChildren(params:Category[]) {
+    return(
+      params.map((param)=>{
+        <CategoryItem {...param}></CategoryItem>
+        if(!param.collapse){
+          renderChildren(param.sub)
+        }
+      })
+    )
+  }
+
+  return <div onClick={handListClick(props)}></div>;
 }
 
 export default CategoryList;
