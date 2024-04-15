883d3be1c1485c4a56c1e9cc815edbde52c72e43
dliang <humga@icloud.com>
2024-04-15 22:01:08 +0800

    fetchdata

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,13 +1,39 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 // import { CategoryResp } from "../data/Category";
+import { Router, Route, Link } from "react-router-dom";
 import { Category } from "../data/Category";
 import CategoryRoot from "./CategoryRoot";
 import { MenuIcon } from "../common/Icon";
 
 const Menu = () => {
   const [category, setCategory] = useState<[Category]>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleMenuClick = useCallback(() => {
+  useEffect(() => {
+     const fetchData = async ()=>{
+      try {
+        setIsLoading(true);
+        const response = await fetch('http://localhost:8000/get_categorys');
+        if (!response.ok){
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        setCategory(jsonData);
+
+      } catch (err:any) {
+        setError(err.message);
+      }finally{
+        setIsLoading(false);
+      }
+     }
+
+     fetchData();
+
+     alert(category);
+  }, []);
+
+  const handleMenuCloseClick = useCallback(() => {
     category?.map((item, index) => {
       alert(item);
       alert(index);
@@ -22,7 +48,7 @@ const Menu = () => {
   return (
     <>
       <div>
-        <MenuIcon onClick={handleMenuClick} />
+        <MenuIcon onClick={handleMenuCloseClick} />
       </div>
       <div>
         <CategoryRoot
