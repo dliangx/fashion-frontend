53356085018821cc7fde5f9d9be903b8ef87b507
dliang <humga@icloud.com>
2024-04-20 13:20:43 +0800

    header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,15 @@
+import { MenuIcon, Search, ShoppingBag } from "./Icon";
+import { Logo } from "./Logo";
+
 const Header = () => {
-  <></>;
+  return (
+    <div className="flex h-10 mt-2 items-end">
+      <MenuIcon className="ml-4" />
+      <Logo className="h-8 m-auto" />
+      <Search className="mr-4" />
+      <ShoppingBag className="mr-4" />
+    </div>
+  );
 };
 
 export default Header;
