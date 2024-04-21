5f562ca7c73a6c5df6079cd3930dd81bdfb094f4
dliang <humga@icloud.com>
2024-04-21 21:40:51 +0800

    cart footer

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,5 +1,19 @@
+import { Close, ShoppingBag } from "../common/Icon";
+
 const Cart = () => {
-  return <></>;
+  return (
+    <div className="h-full">
+      <Close className="m-2" onClick={() => history.go(-1)} />
+      <div className="m-3">CART</div>
+
+      <div className="fixed bottom-0 left-0 right-0 bg-black text-color=#fff h-12 pt-3 text-center ">
+        <div>
+          <ShoppingBag className="absolute top-2 left-20 z-20" />
+          CONTINUE SHOPPING
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
