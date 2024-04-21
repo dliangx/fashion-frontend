292d0a68d6bbba4f3f50377d034ce556fcce73e8
dliang <humga@icloud.com>
2024-04-21 23:26:08 +0800

    empty cart

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -5,6 +5,9 @@ const Cart = () => {
     <div className="h-full">
       <Close className="m-2" onClick={() => history.go(-1)} />
       <div className="m-3">CART</div>
+      <div className="grid place-items-center min-h-screen pb-12">
+        <div>You have no items in you Shopping Bag.</div>
+      </div>
 
       <div className="fixed bottom-0 left-0 right-0 bg-black text-color=#fff h-12 pt-3 text-center text-white ">
         <div>
