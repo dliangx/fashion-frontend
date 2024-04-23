f2cfb0b7b20e05c42c3ca06fe4561184dbcebe4f
dliang <humga@icloud.com>
2024-04-23 23:16:20 +0800

    NewArrival

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,14 @@
 import Footer from "../common/Footer";
 import Header from "../common/Header";
 import Collections from "./Collections";
+import NewArrival from "./NewArrival";
 
 const Home = () => {
   return (
     <div className="h-full">
       <Header />
       <Collections />
+      <NewArrival />
       <Footer />
     </div>
   );
--- src/components/home/NewArrival.tsx
+++ src/components/home/NewArrival.tsx
@@ -1,5 +1,12 @@
 const NewArrival = () => {
-  <></>;
+  return (
+    <>
+      <div className="grid place-items-center mt-10 mb-8">
+        <h1 className="text-center text-xl font-serif">NEW ARRIVAL</h1>
+        <img src="/assets/underline.svg"></img>
+      </div>
+    </>
+  );
 };
 
 export default NewArrival;
