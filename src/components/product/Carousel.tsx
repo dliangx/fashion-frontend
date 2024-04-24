33278066e978f06126c807b0051c266a075e653c
dliang <humga@icloud.com>
2024-04-24 19:37:08 +0800

    add touch event

--- src/components/product/Carousel.tsx
+++ src/components/product/Carousel.tsx
@@ -1,28 +1,77 @@
 // Carousel.tsx
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { ImageSlide } from "../data/Product";
 
 const Carousel: React.FC<{ images: ImageSlide[] }> = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const touchStartX = useRef(0);
+  const isDragging = useRef(false);
+  let dx = 0;
+  const slideWidth = carouselRef.current ? carouselRef.current.offsetWidth : 0;
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
+  const goToSlide = (slideIndex: number) => {
+    setCurrentIndex(slideIndex);
+    isDragging.current = false;
+  };
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, [images.length]);
+  const dragStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (carouselRef.current) {
+      touchStartX.current = e.touches[0].clientX;
+
+      isDragging.current = true;
+    }
+  };
+
+  const dragMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (isDragging.current) {
+      dx = e.touches[0].clientX - touchStartX.current;
+      //   if (carouselRef.current != null)
+      //     carouselRef.current.style.transform = `translateX( ${dx}px)`;
+    }
+  };
+
+  const dragEnd = () => {
+    if (isDragging.current) {
+      const threshold = slideWidth / 2;
+
+      if (Math.abs(dx) > threshold) {
+        setCurrentIndex(
+          (prevIndex) => (prevIndex + (dx > 0 ? -1 : 1)) % images.length
+        );
+      }
+      if (carouselRef.current != null)
+        carouselRef.current.style.transform = `translateX(-${
+          currentIndex * slideWidth
+        }px)`;
+      if (carouselRef.current != null)
+        carouselRef.current.style.transition = "transform 0.5s ease-out";
+    }
+    isDragging.current = false;
+  };
 
   return (
     <div className=" relative w-full overflow-hidden">
       <div
+        ref={carouselRef}
+        onTouchStart={dragStart}
+        onTouchMove={dragMove}
+        onTouchEnd={dragEnd}
         className="flex transition duration-500 ease-in-out"
         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
       >
-        {images.map((slide) => (
-          <img src={slide.src} alt={slide.alt} className="w-full" />
+        {images.map((slide, index) => (
+          <div
+            key={index}
+            className="flex-none w-full snap-center"
+            onClick={() => goToSlide((index + 1) % images.length)}
+          >
+            <img
+              src={slide.src}
+              alt={slide.alt}
+              className="w-full object-cover"
+            />
+          </div>
         ))}
       </div>
     </div>
