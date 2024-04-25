7331a32e3488db88a63c482c472054b6064bb2dc
dliang <humga@icloud.com>
2024-04-25 10:37:49 +0800

    new arrival

--- src/components/home/NewArrival.tsx
+++ src/components/home/NewArrival.tsx
@@ -1,4 +1,24 @@
+import { useContext, useEffect } from "react";
+import { AppContext } from "../../App";
+
 const NewArrival = () => {
+  const { newProducts, setNewProducts } = useContext(AppContext);
+  useEffect(() => {
+    if (newProducts.length == 0) {
+      fetch(import.meta.env.VITE_API_URL + "/home_new_product", {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setNewProducts(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  }, []);
+
   return (
     <>
       <div className="grid place-items-center mt-10 mb-8">
