28cd57d28a19e5852ea534b4292fe8929e33ff8b
dliang <humga@icloud.com>
2024-04-26 00:13:14 +0800

    productGrid style

--- src/components/product/ProductGrid.tsx
+++ src/components/product/ProductGrid.tsx
@@ -4,7 +4,7 @@ const ProductGrid = (props: ProductInfo) => {
   return (
     <div className="grid  m-2">
       <img src={props.pic} alt={props.name} onClick={() => {}} />
-      <div className="text-xl font-mono">{props.brand}</div>
+      <div className="font-serif text-lg">{props.brand}</div>
       <div className="font-sans">{props.name}</div>
       <div className="font-sans">${props.price}</div>
     </div>
