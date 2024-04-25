0868df22817d794bc902ddb5bf88b00fb19afa2d
dliang <humga@icloud.com>
2024-04-25 17:44:34 +0800

    new arrival

--- src/components/home/NewArrival.tsx
+++ src/components/home/NewArrival.tsx
@@ -1,8 +1,11 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../App";
+import { ProductInfo } from "../data/Product";
+import Products from "../product/Products";
 
 const NewArrival = () => {
   const { newProducts, setNewProducts } = useContext(AppContext);
+
   useEffect(() => {
     if (newProducts.length == 0) {
       fetch(import.meta.env.VITE_API_URL + "/home_new_product", {
@@ -11,7 +14,21 @@ const NewArrival = () => {
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          setNewProducts(data);
+          const products = data.map((element: any) => {
+            const product: ProductInfo = {
+              id: element.product_id,
+              name: element.product_name,
+              category: element.category,
+              pic: element.pic,
+              brand: element.brand,
+              rating: element.rating,
+              price: element.price,
+            };
+
+            return product;
+          });
+
+          setNewProducts(products);
         })
         .catch((error) => {
           console.error("Error:", error);
@@ -24,6 +41,9 @@ const NewArrival = () => {
       <div className="grid place-items-center mt-10 mb-8">
         <h1 className="text-center text-xl font-serif">NEW ARRIVAL</h1>
         <img src="/assets/underline.svg"></img>
+        <div>
+          <Products products={newProducts} option={3}></Products>
+        </div>
       </div>
     </>
   );
