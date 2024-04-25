733a915619786cd88114ee09bac5135026a98409
dliang <humga@icloud.com>
2024-04-25 15:09:55 +0800

    update style

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   const [tabIndex, setTabIndex] = useState<number>(1);
   return (
-    <div className="fixed bottom-0 left-0 right-0  h-14 pt-4 text-center bg-black text-white  border-none phone-width  ">
+    <div className="fixed bottom-0 left-0 right-0  h-16 pt-2 text-center bg-black text-white  border-none phone-width  ">
       <div className="flex">
         <div
           className="w-1/4 grid place-items-center"
@@ -15,7 +15,7 @@ const Footer = () => {
           <Link to="/">
             <div className="h-6 text-bottom">HOME</div>
           </Link>
-          <div className="h-10">
+          <div className="h-12">
             {1 === tabIndex && <img src="/assets/rect.svg" />}
           </div>
         </div>
@@ -54,7 +54,7 @@ const Footer = () => {
           <Link to="/out_story">
             <div className="h-6 ">ME</div>
           </Link>
-          <div className="h-10">
+          <div className="h-12">
             {4 === tabIndex && <img src="/assets/rect.svg" />}
           </div>
         </div>
