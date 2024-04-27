1c8d0051c4a51de8c38a95cc97f515345f4e2d0c
dliang <humga@icloud.com>
2024-04-27 13:29:23 +0800

    product add navigate

--- src/components/product/ProductGrid.tsx
+++ src/components/product/ProductGrid.tsx
@@ -1,9 +1,21 @@
+import { useContext } from "react";
 import { ProductInfo } from "../data/Product";
+import { AppContext } from "../../App";
+import { useNavigate } from "react-router-dom";
 
 const ProductGrid = (props: ProductInfo) => {
+  const { setTabIndex } = useContext(AppContext);
+  const navigate = useNavigate();
   return (
     <div className="grid  m-2">
-      <img src={props.pic} alt={props.name} onClick={() => {}} />
+      <img
+        src={props.pic}
+        alt={props.name}
+        onClick={() => {
+          setTabIndex(3);
+          navigate("/product/" + props.id.toString());
+        }}
+      />
       <div className="font-serif text-lg">{props.brand}</div>
       <div className="font-sans">{props.name}</div>
       <div className="font-sans">${props.price}</div>
--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,12 +1,24 @@
+import { useContext } from "react";
 import { Heart } from "../common/Icon";
 import { ProductInfo } from "../data/Product";
+import { AppContext } from "../../App";
+import { useNavigate } from "react-router-dom";
 
 const ProductList = (props: ProductInfo) => {
+  const { setTabIndex } = useContext(AppContext);
+  const navigate = useNavigate();
   return (
     <>
       <div className="flex w-full m-2">
         <div className="h32 w-1/4">
-          <img src={props.pic} alt={props.name} onClick={() => {}} />
+          <img
+            src={props.pic}
+            alt={props.name}
+            onClick={() => {
+              setTabIndex(3);
+              navigate("/product/" + props.id.toString());
+            }}
+          />
         </div>
         <div className="h32 w-3/4 ml-2">
           <div className="font-mono text-xl  mt-4">{props.brand}</div>
