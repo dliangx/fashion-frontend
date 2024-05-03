ebbf4c7dd9dee5562d0b85b34db836424d14794f
dliang <humga@icloud.com>
2024-05-03 21:26:41 +0800

    search param

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,4 +1,7 @@
+import { useContext, useState } from "react";
 import { Close, SearchIcon } from "../common/Icon";
+import { AppContext } from "../../App";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
   return (
@@ -10,15 +13,31 @@ const Search = () => {
 };
 
 export const SearchInput = () => {
+  const { setTabIndex } = useContext(AppContext);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
   return (
     <div className="relative">
       <input
         placeholder="Search Items"
         type="text"
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+        }}
         className="w-11/12 m-4 h-10 bg-transparent p-2 pl-8 border-b border-gray-600 hover:border-gray-300 focus:outline-none "
         autoFocus
       ></input>
-      <SearchIcon className="absolute top-6 left-4 z-10" />
+      <SearchIcon
+        className="absolute top-6 left-4 z-10"
+        onClick={() => {
+          setTabIndex(3);
+          navigate("/product", {
+            state: {
+              search: searchValue,
+            },
+          });
+        }}
+      />
       <Close
         className="absolute top-6 right-4 z-20"
         onClick={() => history.go(-1)}
