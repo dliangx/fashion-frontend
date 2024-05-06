7f4b12d19eb94404ae6df6fe06e47b1b22fdd6a0
dliang <humga@icloud.com>
2024-05-06 23:01:32 +0800

    rm unused

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Close, Heart, ShoppingBag } from "../common/Icon";
 import { AppContext } from "../../App";
 import { CartContext } from "./CartContext";
