69addf758d234ff8c943a95d76bc4167e357a16e
dliang <humga@icloud.com>
2024-05-07 01:22:12 +0800

    attributes select event

---
+++ src/components/product/Attributes.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+export type Attr = {
+  name: string;
+  values: string[];
+};
+
+const Attributes = (attr: Attr) => {
+  const [selectIndex, setSelectIndex] = useState(-1);
+  return (
+    <div className="flex ">
+      <h2 className="mr-4 font-sans">{attr.name}</h2>
+
+      {attr.values.map((value, attrIndex) => {
+        return (
+          <div className="mr-4" key={value}>
+            {attr.name == "Color" && (
+              <>
+                <button
+                  className={`w-4 h-4  rounded-full border  
+                            ${value === "black" ? " bg-black" : "bg-black"} 
+                            ${
+                              value === "orange" ? " bg-orange-500" : "bg-black"
+                            } 
+                              ${value === "white" ? " bg-gray-200" : "bg-black"}
+                              ${value.startsWith("#") ? value : "bg-black"} 
+                              ${
+                                selectIndex == attrIndex
+                                  ? " border-orange-700"
+                                  : " border-white"
+                              }
+                            `}
+                  onClick={() => {
+                    setSelectIndex(attrIndex);
+                  }}
+                ></button>
+              </>
+            )}
+            {attr.name != "Color" && (
+              <button
+                className={`w-5 h-5  rounded-full border text-xs  align-text-top
+                        ${
+                          selectIndex == attrIndex
+                            ? " border-orange-700"
+                            : " border-white"
+                        }
+                        `}
+                onClick={() => {
+                  setSelectIndex(attrIndex);
+                }}
+              >
+                {value}
+              </button>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Attributes;
--- src/components/product/ProductAttribute.tsx
+++ src/components/product/ProductAttribute.tsx
@@ -1,13 +1,9 @@
 import { useEffect, useState } from "react";
 import { AttributeProps } from "../data/Product";
-
-type attr = {
-  name: string;
-  values: string[];
-};
+import Attributes, { Attr } from "./Attributes";
 
 const ProductAttribute = (props: AttributeProps) => {
-  const [attrs, setAttrs] = useState<attr[]>([]);
+  const [attrs, setAttrs] = useState<Attr[]>([]);
 
   useEffect(() => {
     let attrRes = [];
@@ -19,7 +15,7 @@ const ProductAttribute = (props: AttributeProps) => {
         attr_name_cache = element.name;
         attr_cache.push(element.value);
       } else if (attr_name_cache != element.name) {
-        const attr1: attr = { name: attr_name_cache, values: attr_cache };
+        const attr1: Attr = { name: attr_name_cache, values: attr_cache };
         attrRes.push(attr1);
         attr_cache = [];
         attr_name_cache = element.name;
@@ -28,7 +24,7 @@ const ProductAttribute = (props: AttributeProps) => {
         attr_cache.push(element.value);
       }
     }
-    const attr2: attr = { name: attr_name_cache, values: attr_cache };
+    const attr2: Attr = { name: attr_name_cache, values: attr_cache };
     attrRes.push(attr2);
     setAttrs(attrRes);
   }, [props]);
@@ -36,39 +32,8 @@ const ProductAttribute = (props: AttributeProps) => {
   return (
     <>
       <div className="flex flex-wrap">
-        {attrs.map((attr, index) => {
-          return (
-            <div className="flex " key={index.toString()}>
-              <h2 className="mr-4 font-sans">{attr.name}</h2>
-
-              {attr.values.map((value) => {
-                return (
-                  <div className="mr-4" key={value}>
-                    {attr.name == "Color" && (
-                      <>
-                        <button
-                          className={`w-4 h-4  rounded-full border  
-                            ${value === "black" ? " bg-black" : "bg-black"} 
-                            ${
-                              value === "orange" ? " bg-orange-500" : "bg-black"
-                            } 
-                              ${
-                                value === "white" ? " bg-gray-200" : "bg-black"
-                              } 
-                            `}
-                        ></button>
-                      </>
-                    )}
-                    {attr.name != "Color" && (
-                      <button className="w-5 h-5  rounded-full border text-xs  align-text-top">
-                        {value}
-                      </button>
-                    )}
-                  </div>
-                );
-              })}
-            </div>
-          );
+        {attrs.map((attr) => {
+          return <Attributes {...attr} />;
         })}
       </div>
     </>
