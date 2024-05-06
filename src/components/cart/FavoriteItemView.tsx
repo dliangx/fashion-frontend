144ccf93e1cb9b05708070f582fe01fff3ec9edb
dliang <humga@icloud.com>
2024-05-06 14:23:13 +0800

    favorite item remove

--- src/components/cart/FavoriteItemView.tsx
+++ src/components/cart/FavoriteItemView.tsx
@@ -1,11 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FavoriteItem } from "../data/Cart";
 import { AppContext } from "../../App";
 import { useNavigate } from "react-router-dom";
+import { Heart } from "../common/Icon";
+import { FavoriteContext } from "./FavoriteContext";
 
 const FavoriteItemView = (props: FavoriteItem) => {
   const { setTabIndex } = useContext(AppContext);
   const navigate = useNavigate();
+  const { dispatch } = useContext(FavoriteContext);
+  const [isFavorite, setIsFavorite] = useState(true);
+
   return (
     <>
       <div className="flex w-full m-2">
@@ -23,6 +28,31 @@ const FavoriteItemView = (props: FavoriteItem) => {
           <div className="font-mono text-xl  mt-4">{props.brand}</div>
           <div className="font-sans">{props.name}</div>
           <div className="font-sans text-orange-500">${props.price}</div>
+          <Heart
+            className="mt-4 right-2 bottom-2 h-4 "
+            color="#ff4700"
+            fill={isFavorite ? "#ff4700" : "none"}
+            onClick={() => {
+              if (localStorage.getItem("user_token") == undefined) {
+                navigate("/login");
+
+                return;
+              }
+
+              dispatch != null &&
+                dispatch({
+                  type: "REMOVE",
+                  payload: {
+                    id: props.id,
+                    pic: props.pic,
+                    brand: props.brand,
+                    name: props.name,
+                    price: props.price,
+                  },
+                });
+              setIsFavorite(false);
+            }}
+          />
         </div>
       </div>
     </>
