b2204be54bbe3afa21c39b00a44decaef2c0ad40
dliang <humga@icloud.com>
2024-05-07 23:34:05 +0800

    checkout

--- src/components/order/Checkout.tsx
+++ src/components/order/Checkout.tsx
@@ -1,5 +1,19 @@
+import Header from "../common/Header";
+import { ShoppingBag } from "../common/Icon";
+
 const Checkout = () => {
-  return <></>;
+  return (
+    <div>
+      <Header />
+
+      <div className="fixed bottom-0 left-0 right-0 h-14 bg-black text-white border-none phone-width   place-content-center">
+        <div className="flex  text-center  place-content-center">
+          <ShoppingBag className="mr-4" />
+          <div>CHECKOUT</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
