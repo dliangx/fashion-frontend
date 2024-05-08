9f400a4edb39c759b2cf882cacf9198c5dcaee25
dliang <humga@icloud.com>
2024-05-08 08:35:20 +0800

    text color

--- src/components/product/AlertModal.tsx
+++ src/components/product/AlertModal.tsx
@@ -4,7 +4,7 @@ const AlertModal = (props: { content: string; onClose: any }) => {
   return (
     <div className="fixed flex bg-black bottom-14 left-0 right-0 ">
       <Close className="m-4" onClick={props.onClose} color="white" />
-      <div className="h-10 text-center  place-content-center mt-2">
+      <div className="h-10 text-center text-white  place-content-center mt-2">
         {props.content}
       </div>
     </div>
