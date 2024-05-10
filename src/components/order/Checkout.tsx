4f590616f9389a861ac9087c4a1dd4d2788a1a72
dliang <humga@icloud.com>
2024-05-10 21:28:13 +0800

    payment success page

--- src/components/order/Checkout.tsx
+++ src/components/order/Checkout.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import Header from "../common/Header";
 import { ShoppingBag } from "../common/Icon";
+import PaymentSuccess from "./PaymentSuccess";
 
 const Checkout = () => {
+  const [isSuccess, setIsSuccess] = useState(false);
   return (
     <div>
       <Header />
-
-      <div className="fixed bottom-0 left-0 right-0 h-14 bg-black text-white border-none phone-width   place-content-center">
-        <div className="flex  text-center  place-content-center">
+      {isSuccess && (
+        <PaymentSuccess
+          onclick={() => {
+            setIsSuccess(false);
+          }}
+        />
+      )}
+      <div className="fixed bottom-0 left-0 right-0 h-14 bg-black text-white border-none phone-width   place-content-center z-50">
+        <div
+          className="flex  text-center  place-content-center"
+          onClick={() => {
+            setIsSuccess(true);
+          }}
+        >
           <ShoppingBag className="mr-4" color="white" />
           <div>CHECKOUT</div>
         </div>
--- src/components/order/PaymentSuccess.tsx
+++ src/components/order/PaymentSuccess.tsx
@@ -1,5 +1,32 @@
-const PaymentSuccess = () => {
-  <></>;
+import { Close } from "../common/Icon";
+
+const PaymentSuccess = (props: { onclick: any }) => {
+  return (
+    <div className="absolute bottom-0 top-0 left-0 right-0 bg-black  bg-opacity-80 phone-width  z-10">
+      <div className="mt-20 m-4 pt-4 alert ">
+        <Close className="ml-4" onClick={props.onclick} />
+        <div className="grid place-items-center mt-10 mb-8">
+          <h1 className="text-center text-xl ">PAYMENT SUCCESS</h1>
+        </div>
+        <div className="grid place-items-center">
+          <img src="/assets/success.svg" />
+          <div className="mt-8"> Your payment was success </div>
+          <div className="mt-2">Payment ID </div>
+          <img className="m-4" src="/assets/underline.svg"></img>
+          <div>Rate you purchase</div>
+          <div className="flex mt-2  space-x-2">
+            <img src="/assets/rate1.svg" />
+            <img src="/assets/rate2.svg" />
+            <img src="/assets/rate3.svg" />
+          </div>
+          <div className="flex w-full p-4">
+            <button className="w-1/2 m-2 h-10">SUBMIT</button>
+            <button className="w-1/2 m-2 h-10">BACK TO HOME</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentSuccess;
